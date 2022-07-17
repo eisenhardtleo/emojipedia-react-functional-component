8f115726c8f913082211aebac6e1484b982bf9f5
eisenhardt.leo <latveria0@gmail.com>
2022-07-17 19:04:10 +0200

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,58 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "While this emoji is often used by people calling themselves nerds in a self-deprecating way, the nerdy glasses paired with buck teeth can, and often does, evoke anti-Asian stereotypes."
+  },
+  {
+    id: 5,
+    emoji: "🥰",
+    name: "Smiling Face with Hearts",
+    meaning:
+      "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love."
+  },
+  {
+    id: 6,
+    emoji: "😠",
+    name: "Angry, grumpy face",
+    meaning:
+      "A yellow face with a frowning mouth and eyes and eyebrows scrunched downward in anger. Conveys varying degrees of anger, from grumpiness and irritation to disgust and outrage. May also represent someone acting tough or being mean."
+  },{
+    id: 7,
+    emoji: "😮",
+    name: "Face with Open Mouth",
+    meaning:
+      "A yellow face with small, open eyes and a large, round mouth, slack with surprise or shock, as if saying Wow! or Oh my! May convey such feelings as awe or disbelief."
+  },{
+    id: 8,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning:
+      "A yellow face with furrowed eyebrows looking upwards with thumb and index finger resting on its chin. Intended to show a person pondering or deep in thought. Often used to question or scorn something or someone, as if saying Hmm, I don't know about that."
+  }
+];
+
+export default emojipedia;
